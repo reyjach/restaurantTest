05f0a83171d8a3f2732880dbde6289650c138450
reyjach <reymuerto14@hotmail.com>
2021-03-12 02:34:10 -0500

    add save the form in the state

--- components/restaurants/AddRestaurantsForm.js
+++ components/restaurants/AddRestaurantsForm.js
@@ -5,13 +5,30 @@ import CountryPicker from 'react-native-country-picker-modal'
 
 export default function AddRestaurantsForm( {toastRef, setLoading, navigation} ) {
 
+    const [formData, setFormData] = useState(defaulFormValue())
+    const [errorName, setErrorName] = useState(null)
+    const [errorDescription, setErrorDescription] = useState(null)
+    const [errorEmail, setErrorEmail] = useState(null)
+    const [errorAddress, setErrorAddress] = useState(null)
+    const [errorPhone, setErrorPhone] = useState(null)
+
     const addRestaurant = () => {
+        console.log(formData)
         console.log("hola")
     }
 
     return (
         <View style={styles.viewContainer}>
-            <FormApp></FormApp>
+            <FormApp
+                formData={formData}
+                setFormData={setFormData}
+                errorName={errorName}
+                errorDescription={errorDescription}
+                errorEmail={errorEmail}
+                errorAddress={errorAddress}
+                errorPhone={errorPhone}
+            >
+            </FormApp>
             <Button
                 title="Crear restaurante"
                 onPress={addRestaurant}
@@ -22,24 +39,37 @@ export default function AddRestaurantsForm( {toastRef, setLoading, navigation} )
     )
 }
 
-function FormApp() {
+function FormApp({ formData, setFormData, errorName, errorDescription, errorEmail, errorAddress, errorPhone}) {
     const [country, setCountry] = useState("CO")
     const [callingCodo, setCallingCodo] = useState("57")
     const [phone, setPhone] = useState("")
+
+    const onChange = (e, type) => {
+        setFormData({...formData, [type]: e.nativeEvent.text})
+    }
 
     return (
         <View style={styles.viewForm}>
             <Input
                 placeholder="Nombre del restaurante"
+                defaultValue={formData.name}
+                onChange={(e)=> onChange(e, "name")}
+                errorMessage={errorName}
             >
             </Input>
             <Input
                 placeholder="Direccion del restaurante"
+                defaultValue={formData.address}
+                onChange={(e)=> onChange(e, "address")}
+                errorMessage={errorAddress}
             >
             </Input>
             <Input
                 keyboardType="email-address"
                 placeholder="Email del restaurante"
+                defaultValue={formData.email}
+                onChange={(e)=> onChange(e, "email")}
+                errorMessage={errorEmail}
             >
             </Input>
             <View style={styles.phoneView}>
@@ -51,8 +81,7 @@ function FormApp() {
                     containerStyle={styles.countryPicker}
                     countryCode={country}
                     onSelect={(country) => {
-                        setCountry(country.cca2)
-                        setCallingCodo(country.callingCode[0])
+                        setFormData({...formData, country: country.cca2, callingCode: country.callingCode[0]})
                     }}
                 >
                 </CountryPicker>
@@ -60,17 +89,35 @@ function FormApp() {
                     placeholder="WhatsApp del restaurante"
                     keyboardType="phone-pad"
                     containerStyle={styles.inputPhone}
+                    defaultValue={formData.phone}
+                    onChange={(e)=> onChange(e, "phone")}
+                    errorMessage={errorPhone}
                 >
                 </Input>
             </View>
             <Input
-                    placeholder="Descripcion del restaurante"
-                    multiline
-                    containerStyle={styles.textArea}
-                >
-                </Input>
+                placeholder="Descripcion del restaurante"
+                multiline
+                containerStyle={styles.textArea}
+                defaultValue={formData.description}
+                onChange={(e)=> onChange(e, "description")}
+                errorMessage={errorDescription}
+            >
+            </Input>
         </View>
     )
+}
+
+const defaulFormValue = () => {
+    return {
+        name: "",
+        description: "",
+        email: "",
+        phone: "",
+        address: "",
+        country: "",
+        callingCode: "57"
+    }
 }
 
 const styles = StyleSheet.create({
